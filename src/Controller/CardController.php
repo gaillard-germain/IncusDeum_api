2e0cf838ec1ca37ee0f9d4a6ce3ac2339aaa2658
Gaillard Germain <gaillard.germain@gmail.com>
2022-05-06 15:28:46 +0200

    added category create.

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\{Response, Request};
 use App\Repository\{CardRepository, CategoryRepository};
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,19 +2,36 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{Response, Request};
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CategoryRepository;
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\CategoryType;
 
 class CategoryController extends ApiController
 {
     /**
-     * @Route("/category", name="app_category")
+     * @Route("/category", name="app_category", methods={"GET"})
      */
      public function index(CategoryRepository $categoryRepository): Response
      {
        $categories = $categoryRepository->findAll();
        return $this->normalizeData($categories, ['cards_list']);
+     }
+
+     /**
+     * @Route("/category", name="app_category_create", methods={"POST"})
+     */
+     public function create(Request $request, EntityManagerInterface $manager)
+     {
+       $content = $request->getContent();
+
+       $category = new Category();
+       $category->setName($content);
+       $manager->persist($category);
+       $manager->flush();
+
+       return $this->json($content);
      }
 }
